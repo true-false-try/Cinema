4a6ce37b213a0db5d578d30028fea10a6bdea87c
true-false-try <6mateo6ch@gmail.com>
2023-03-21 22:21:01 +0200

    recreated code for security, and was caught branch as swagger

--- src/main/java/com/logic/cinema/config/WebSecurityConfig.java
+++ src/main/java/com/logic/cinema/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.logic.cinema.config;
 
-import com.logic.cinema.model.Role;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -13,6 +12,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
+import static com.logic.cinema.config.constant.SecurityConstant.*;
+import static com.logic.cinema.model.Role.*;
+
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -22,15 +24,19 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
         http
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET,"/api/movies").hasRole(Role.CONSUMER.name())
-                .antMatchers(HttpMethod.POST,"/api/tickets").hasAnyRole(Role.CONSUMER.name(), Role.CASHIER.name())
-                .antMatchers(HttpMethod.POST, "/api/movies").hasRole(Role.MODERATOR.name())
-                .antMatchers(HttpMethod.PUT, "/api/movies").hasRole(Role.MODERATOR.name())
-                .antMatchers(HttpMethod.GET, "/api/tickets").hasRole(Role.CASHIER.name())
-                .antMatchers(HttpMethod.DELETE, "/api/tickets").hasRole(Role.SUPPORT_L1.name())
-                .antMatchers(HttpMethod.POST, "/api/halls").hasRole(Role.SUPPORT_L2.name())
-                .antMatchers(HttpMethod.GET, "/api/**").hasRole(Role.ADMIN.name())
-                .antMatchers(HttpMethod.POST, "/api/**").hasRole(Role.ADMIN.name())
+                .mvcMatchers(HttpMethod.GET, API_PATH_TO_MOVIES.getName()).hasAnyRole(CONSUMER.name(), MODERATOR.name())
+                .mvcMatchers(HttpMethod.POST, API_PATH_TO_TICKETS.getName()).hasAnyRole(CONSUMER.name(), CASHIER.name())
+                .mvcMatchers(HttpMethod.POST, API_PATH_TO_MOVIES.getName()).hasRole(MODERATOR.name())
+                .mvcMatchers(HttpMethod.PUT, API_PATH_TO_MOVIES.getName()).hasRole(MODERATOR.name())
+                .mvcMatchers(HttpMethod.GET, API_PATH_TO_TICKETS.getName()).hasAnyRole(CONSUMER.name(), CASHIER.name())
+                .mvcMatchers(HttpMethod.DELETE, API_PATH_TO_TICKETS.getName()).hasRole(SUPPORT_L1.name())
+                .mvcMatchers(HttpMethod.POST, API_PATH_TO_HALLS.getName()).hasRole(SUPPORT_L2.name())
+
+                //Admin
+                .mvcMatchers(HttpMethod.GET, ALL_API_PATH.getName()).hasRole(ADMIN.name())
+                .mvcMatchers(HttpMethod.POST, ALL_API_PATH.getName()).hasRole(ADMIN.name())
+                .mvcMatchers(HttpMethod.PUT, ALL_API_PATH.getName()).hasRole(ADMIN.name())
+                .mvcMatchers(HttpMethod.DELETE, ALL_API_PATH.getName()).hasRole(ADMIN.name())
                 .anyRequest()
                 .authenticated()
                 .and()
@@ -46,32 +52,32 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 User.builder()
                         .username("consumer")
                         .password(passwordEncoder().encode("qwerty"))
-                        .roles(Role.CONSUMER.name())
+                        .roles(CONSUMER.name())
                         .build(),
                 User.builder()
                         .username("moderator")
                         .password(passwordEncoder().encode("qwerty"))
-                        .roles(Role.MODERATOR.name())
+                        .roles(MODERATOR.name())
                         .build(),
                 User.builder()
                         .username("cashier")
                         .password(passwordEncoder().encode("qwerty"))
-                        .roles(Role.CASHIER.name())
+                        .roles(CASHIER.name())
                         .build(),
                 User.builder()
                         .username("support_l1")
                         .password(passwordEncoder().encode("qwerty"))
-                        .roles(Role.SUPPORT_L1.name())
+                        .roles(SUPPORT_L1.name())
                         .build(),
                 User.builder()
                         .username("support_l2")
                         .password(passwordEncoder().encode("qwerty"))
-                        .roles(Role.SUPPORT_L2.name())
+                        .roles(SUPPORT_L2.name())
                         .build(),
                 User.builder()
                         .username("admin")
                         .password(passwordEncoder().encode("qwerty"))
-                        .roles(Role.ADMIN.name())
+                        .roles(ADMIN.name())
                         .build());
 
     }
